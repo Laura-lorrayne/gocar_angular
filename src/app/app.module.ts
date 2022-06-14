6fb1f104836f0fdfc21dc42e11c4016927e483a9
Laura Lorrayne <lauralorrayne24@gmail.com>
2022-06-13 22:12:42 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from "../components/login/login.component"
+import { HomeComponent } from "../components/home/home.component"
+import { CadastroComponent } from "../components/cadastro/cadastro.component"
+import { CatalogoComponent } from "../components/catalogo/catalogo.component"
+import { SimularComponent } from "../components/simular/simular.component"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    CadastroComponent,
+    CatalogoComponent,
+    SimularComponent
+
   ],
   imports: [
     BrowserModule
